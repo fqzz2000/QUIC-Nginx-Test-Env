8beee250c967b492ff5b8f85e09eca6ea78c7190
fqzz2000 <fqzz2000@outlook.com>
2025-04-10 23:42:05 -0400

    possbly working version for cross node testing by updating the script to use image on dockerhub

--- start-h3-lb.sh
+++ start-h3-lb.sh
@@ -1,1 +1,1 @@
-docker run --name nginx-frontend-http3 --rm  --network host   -v $(pwd)/frontend/nginx-http3.conf:/etc/nginx/nginx.conf   -v $(pwd)/certs:/etc/nginx/certs   -d nginx-frontend-test
+docker run --name nginx-frontend-http3 --rm  --network host   -v $(pwd)/frontend/nginx-http3.conf:/etc/nginx/nginx.conf   -v $(pwd)/certs:/etc/nginx/certs   -d fqzz2000/nginx-frontend
