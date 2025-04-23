f1cd07ef1d3941692915b2fa064af70507c49ae4
Quanzhi <fqzz2000@outlook.com>
2025-04-22 23:06:35 -0400

    update nginx yaml

--- start-h3-lb.sh
+++ start-h3-lb.sh
@@ -1,4 +1,9 @@
-docker run --name envoy-proxy --network host \
+docker run --rm --name envoy-proxy --network host \
   -v $(pwd)/frontend/envoy.yaml:/etc/envoy/envoy.yaml \
-  -v $(pwd)/certs:/etc/envoy/certs \
-  -d envoy-proxy-image
+  -v $(pwd)/cert-envoy:/etc/envoy/certs \
+  -d envoyproxy/envoy:v1.26-latest
+
+# docker run --rm --name envoy-proxy --network host \
+#   -v $(pwd)/cert-envoy:/etc/envoy/certs \
+#   -d envoyproxy/envoy:v1.26-latest
+
